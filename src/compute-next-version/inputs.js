30a9b61bece619b649cca460c0aa25931d9d57f4
Josh Cummings <3627351+jzheaux@users.noreply.github.com>
2026-01-14 13:18:20 -0700

    Add Action for Computing Next Version and Release Date
    Adds compute-next-version action that computes the next milestone
    release name and scheduled date. This action queries both the
    repository and website content to determine the next version, its
    due date, and version type (OSS or enterprise).
    
    Signed-off-by: Josh Cummings <3627351+jzheaux@users.noreply.github.com>
    
    Closes gh-8

---
+++ src/compute-next-version/inputs.js
@@ -0,0 +1,55 @@
+const core = require("@actions/core");
+
+class Inputs {
+	constructor() {
+		const repository = process.env.GITHUB_REPOSITORY.split("/")[1];
+		const commercial = repository.endsWith("-commercial");
+		this._version = core.getInput("version", { required: true });
+		this._token = core.getInput("token", {
+			required: true,
+		});
+		this._websiteToken = core.getInput("website-token", { required: true });
+		this._repository =
+			core.getInput("repository") || process.env.GITHUB_REPOSITORY;
+		let websiteRepository = core.getInput("website-repository");
+		if (!websiteRepository) {
+			websiteRepository = commercial
+				? "spring-io/spring-website-commercial-content"
+				: "spring-io/spring-website-content";
+		}
+		let projectSlug = core.getInput("project-slug");
+		if (!projectSlug) {
+			projectSlug = repository.replace("-commercial", "");
+		}
+		this._projectSlug = projectSlug;
+		this._websiteRepository = websiteRepository;
+	}
+
+	get version() {
+		return this._version;
+	}
+
+	get token() {
+		return this._token;
+	}
+
+	get websiteToken() {
+		return this._websiteToken;
+	}
+
+	get repository() {
+		return this._repository;
+	}
+
+	get projectSlug() {
+		return this._projectSlug;
+	}
+
+	get websiteRepository() {
+		return this._websiteRepository;
+	}
+}
+
+module.exports = {
+	Inputs,
+};
