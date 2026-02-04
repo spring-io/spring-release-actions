04d0b0282910875e48d097c4011190ccaf037c5a
Josh Cummings <3627351+jzheaux@users.noreply.github.com>
2026-02-04 15:05:01 -0700

    Fix OSS and Commercial Date Range Calculation
    This commit corrects boolean logic that incorrectly assumed that
    the current date's year and month being less than the oss end date's year and
    month meant that the current date was in range. This is not true for
    the current year, and so the logic is now adjusted.
    
    Closes gh-47
    
    Signed-off-by: Josh Cummings <3627351+jzheaux@users.noreply.github.com>

--- src/versions.js
+++ src/versions.js
@@ -99,6 +99,7 @@ class Version {
    */
   nextMilestone(generation) {
     if (this.snapshot) {
+      console.log(`returning null since version ${this} is a snapshot version`);
       return null;
     }
     if (this.ga) {
@@ -152,7 +153,10 @@ function _nextGaDate(version, generation) {
   let releaseYear = currentYear + Math.floor(releaseMonth / 12);
   releaseMonth = mod(releaseMonth, 12);
 
-  if (releaseMonth <= oss.end.month && releaseYear <= oss.end.year) {
+  if (
+    releaseYear < oss.end.year ||
+    (releaseYear === oss.end.year && releaseMonth <= oss.end.month)
+  ) {
     const dueDate = getReleaseDate(
       releaseMonth,
       releaseYear,
@@ -170,8 +174,9 @@ function _nextGaDate(version, generation) {
   releaseMonth = mod(releaseMonth, 12);
 
   if (
-    releaseMonth <= enterprise.end.month &&
-    releaseYear <= enterprise.end.year
+    releaseYear < enterprise.end.year ||
+    (releaseYear === enterprise.end.year &&
+      releaseMonth <= enterprise.end.month)
   ) {
     const dueDate = getReleaseDate(
       releaseMonth,
